11b02cc3526d6de98652846f2ac4048e12edf73f
smartcode53 <106484026+smartcode53@users.noreply.github.com>
2022-08-13 12:36:16 +0400

    Initial Commit

---
+++ HilariousDad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HilariousDad
+//
+//  Created by Taha Broachwala on 8/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HilariousDad/HilariousDadApp.swift
@@ -0,0 +1,17 @@
+//
+//  HilariousDadApp.swift
+//  HilariousDad
+//
+//  Created by Taha Broachwala on 8/13/22.
+//
+
+import SwiftUI
+
+@main
+struct HilariousDadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
